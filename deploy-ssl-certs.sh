ae926818681b77d108ead6024311e219bc63ef06
Martin Lowinski <martin@goldtopf.org>
2013-04-01 14:16:06 +0200

    Add script to deploy ssl-certs to nginx/ejabberd.

---
+++ deploy-ssl-certs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ "$(whoami)" != 'root' ]; then
+  echo "Error: $0 requires root/super user access."
+  exit 1;
+fi
+
+###
+# nginx
+###
+echo Deploy certs to nginx..;
+domains=("webmail.halfthetruth.de" "www.goldtopf.org")
+
+for domain in "${domains[@]}"
+do
+  echo Deploying $domain..;
+  cat ${domain}.key ${domain}.crt sub.class1.server.ca.pem > /etc/nginx/ssl/${domain}.pem
+done;
+
+ls -l /etc/nginx/ssl/*.pem
+
+echo "Reloading nginx.."
+/etc/init.d/nginx reload
+
+###
+# ejabberd 
+###
+echo Deploy certs to ejabberd..;
+domains=("www.goldtopf.org")
+
+for domain in "${domains[@]}"
+do
+  echo Deploying $domain..;
+  cat ${domain}.key ${domain}.crt > /etc/ejabberd/${domain}.pem
+done;
+
+ls -l /etc/ejabberd/*.pem
+
+echo "Reloading ejabberd.."
+/etc/init.d/ejabberd restart
+
