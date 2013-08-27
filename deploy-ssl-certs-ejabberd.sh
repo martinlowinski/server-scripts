9851ae5b69cb5835c6aad10076c9bb96fa6b20c9
Martin Lowinski <martin@goldtopf.org>
2013-08-27 13:00:06 +0200

    Add description to scripts.

--- deploy-ssl-certs-ejabberd.sh
+++ deploy-ssl-certs-ejabberd.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+###
+# ejabberd
+#
+# All StartSSL certificates in ~/ have the form <domain>.key and <domain>.crt
+# and are concatinated with the class1-cert into $ejabberd_ssl_dir
+###
+
 if [ "$(whoami)" != 'root' ]; then
   echo "Error: $0 requires root/super user access."
   exit 1;
 fi
 
-###
-# ejabberd 
-###
 echo Deploy certs to ejabberd..;
+
 domains=("www.example.com")
+ejabberd_ssl_dir=/etc/ejabberd
 
 for domain in "${domains[@]}"
 do
   echo Deploying $domain..;
-  cat ${domain}.key ${domain}.crt > /etc/ejabberd/${domain}.pem
+  cat ${domain}.key ${domain}.crt > ${ejabberd_ssl_dir}/${domain}.pem
 done;
 
-ls -l /etc/ejabberd/*.pem
+ls -l ${ejabberd_ssl_dir}/*.pem
 
 echo "Reloading ejabberd.."
-/etc/init.d/ejabberd restart
+/etc/init.d/ejabberd reload
 
--- deploy-ssl-certs-nginx.sh
+++ deploy-ssl-certs-nginx.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+###
+# nginx
+#
+# All StartSSL certificates in ~/ have the form <domain>.key and <domain>.crt
+# and are concatinated with the class1-cert into $nginx_ssl_dir
+###
+
 if [ "$(whoami)" != 'root' ]; then
   echo "Error: $0 requires root/super user access."
   exit 1;
 fi
 
-###
-# nginx
-###
 echo Deploy certs to nginx..;
+
 domains=("www.example.com")
+nginx_ssl_dir=/etc/nginx/ssl
 
 for domain in "${domains[@]}"
 do
   echo Deploying $domain..;
-  cat ${domain}.key ${domain}.crt sub.class1.server.ca.pem > /etc/nginx/ssl/${domain}.pem
+  cat ${domain}.key ${domain}.crt sub.class1.server.ca.pem > ${nginx_ssl_dir}/${domain}.pem
 done;
 
-ls -l /etc/nginx/ssl/*.pem
+ls -l ${nginx_ssl_dir}/*.pem
 
 echo "Reloading nginx.."
 /etc/init.d/nginx reload
